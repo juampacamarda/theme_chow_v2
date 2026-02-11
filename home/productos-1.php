05ef721a93bd32d74fba60466f7e290e41f02778
juampa <49341476+juampacamarda@users.noreply.github.com>
2026-02-10 22:17:51 -0300

    fix: use ul.products with row class for grid layout to match WooCommerce structure

--- home/productos-1.php
+++ home/productos-1.php
@@ -86,14 +86,26 @@
                                     <?php
                                     while ( $products_query->have_posts() ) : $products_query->the_post();
                                         global $product;
+                                        
+                                        // Obtener clases dinámicas como WooCommerce
+                                        $product_class = 'product type-product post-' . get_the_ID() . ' status-publish';
+                                        $product_class .= ' ' . ( $product->is_in_stock() ? 'instock' : 'outofstock' );
+                                        if ( has_post_thumbnail() ) {
+                                            $product_class .= ' has-post-thumbnail';
+                                        }
+                                        $product_class .= ' shipping-taxable purchasable product-type-' . $product->get_type();
+                                        $product_class .= ' product_cat-' . str_replace(' ', '-', strtolower( wc_get_product_category_list( get_the_ID(), ', ' ) ) );
+                                        
+                                        // Clases del botón
+                                        $button_class = 'button product_type_' . $product->get_type() . ' add_to_cart_button ajax_add_to_cart';
                                         ?>
-                                        <li class="product">
-                                            <a href="<?php the_permalink(); ?>" class="woocommerce-loop-item-link">
+                                        <li class="<?php echo esc_attr( $product_class ); ?>">
+                                            <a href="<?php the_permalink(); ?>" class="woocommerce-LoopProduct-link woocommerce-loop-product__link">
                                                 <?php echo get_the_post_thumbnail( get_the_ID(), 'woocommerce_thumbnail', array( 'class' => 'attachment-woocommerce_thumbnail size-woocommerce_thumbnail' ) ); ?>
                                                 <h2 class="woocommerce-loop-product__title"><?php the_title(); ?></h2>
                                                 <span class="price"><?php echo wp_kses_post( $product->get_price_html() ); ?></span>
                                             </a>
-                                            <a href="<?php the_permalink(); ?>" class="button"><?php esc_html_e( 'Ver producto', 'woocommerce' ); ?></a>
+                                            <a href="<?php echo esc_url( add_query_arg( 'add-to-cart', $product->get_id() ) ); ?>" data-quantity="1" class="<?php echo esc_attr( $button_class ); ?>" data-product_id="<?php echo esc_attr( $product->get_id() ); ?>" data-product_sku="<?php echo esc_attr( $product->get_sku() ); ?>" aria-label="Add to cart: &quot;<?php the_title(); ?>&quot;" rel="nofollow"><?php esc_html_e( 'Agregar al carrito', 'woocommerce' ); ?></a>
                                         </li>
                                         <?php
                                     endwhile;
@@ -112,32 +124,41 @@
                             // Layout: Grid de columnas (por defecto) usando estructura WooCommerce
                             if ( $products_query->have_posts() ) :
                                 ?>
-                                <div class="row">
+                                <ul class="products row">
                                     <?php
                                     while ( $products_query->have_posts() ) : $products_query->the_post();
                                         global $product;
+                                        
+                                        // Obtener clases dinámicas como WooCommerce
+                                        $product_class = 'product type-product post-' . get_the_ID() . ' status-publish';
+                                        $product_class .= ' ' . ( $product->is_in_stock() ? 'instock' : 'outofstock' );
+                                        if ( has_post_thumbnail() ) {
+                                            $product_class .= ' has-post-thumbnail';
+                                        }
+                                        $product_class .= ' shipping-taxable purchasable product-type-' . $product->get_type();
+                                        $product_class .= ' product_cat-' . str_replace(' ', '-', strtolower( wc_get_product_category_list( get_the_ID(), ', ' ) ) );
+                                        $product_class .= ' ' . esc_attr( $columnas ) . ' col-md-6 col-sm-12';
+                                        
+                                        // Clases del botón
+                                        $button_class = 'button product_type_' . $product->get_type() . ' add_to_cart_button ajax_add_to_cart';
                                         ?>
-                                        <div class="<?php echo esc_attr( $columnas ); ?> col-md-6 col-sm-12">
-                                            <div class="product">
-                                                <a href="<?php the_permalink(); ?>" class="woocommerce-loop-item-link">
-                                                    <?php echo get_the_post_thumbnail( get_the_ID(), 'woocommerce_thumbnail', array( 'class' => 'attachment-woocommerce_thumbnail size-woocommerce_thumbnail' ) ); ?>
-                                                    <h2 class="woocommerce-loop-product__title"><?php the_title(); ?></h2>
-                                                    <span class="price"><?php echo wp_kses_post( $product->get_price_html() ); ?></span>
-                                                </a>
-                                                <a href="<?php the_permalink(); ?>" class="button"><?php esc_html_e( 'Ver producto', 'woocommerce' ); ?></a>
-                                            </div>
-                                        </div>
+                                        <li class="<?php echo esc_attr( $product_class ); ?>">
+                                            <a href="<?php the_permalink(); ?>" class="woocommerce-LoopProduct-link woocommerce-loop-product__link">
+                                                <?php echo get_the_post_thumbnail( get_the_ID(), 'woocommerce_thumbnail', array( 'class' => 'attachment-woocommerce_thumbnail size-woocommerce_thumbnail' ) ); ?>
+                                                <h2 class="woocommerce-loop-product__title"><?php the_title(); ?></h2>
+                                                <span class="price"><?php echo wp_kses_post( $product->get_price_html() ); ?></span>
+                                            </a>
+                                            <a href="<?php echo esc_url( add_query_arg( 'add-to-cart', $product->get_id() ) ); ?>" data-quantity="1" class="<?php echo esc_attr( $button_class ); ?>" data-product_id="<?php echo esc_attr( $product->get_id() ); ?>" data-product_sku="<?php echo esc_attr( $product->get_sku() ); ?>" aria-label="Add to cart: &quot;<?php the_title(); ?>&quot;" rel="nofollow"><?php esc_html_e( 'Agregar al carrito', 'woocommerce' ); ?></a>
+                                        </li>
                                         <?php
                                     endwhile;
                                     ?>
-                                </div>
+                                </ul>
                                 <?php
                                 wp_reset_postdata();
                             else :
                                 ?>
-                                <div class="col-12">
-                                    <p class="text-center text-muted">No hay productos disponibles en esta sección.</p>
-                                </div>
+                                <p class="text-center text-muted">No hay productos disponibles en esta sección.</p>
                                 <?php
                             endif;
                             ?>
